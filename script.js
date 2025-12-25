a075e5375c8441dbda7e80fb948d7b2554462e64
fahmikemal28-sudo <fahmikemal28@gmail.com>
2025-12-25 22:31:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("Programming koo hojii jalqabeera");
+
+let maqaa = "Fahmi";
+let Umurii = 22;
+
+let biyyaKoo = "Asela";
+
+console.log(
+  "Ani maqaan koo: ",
+  maqaa,
+  " dha. ",
+  "Umuriin koo: ",
+  Umurii,
+  " lafan itti dhaladhe: ",
+  biyyaKoo,
+  " dha.",
+);
+
+// Calculation
+
+console.log("Calculation \n\n");
+
+let umuriiKeeWaggaaDhufuu = Umurii + 1;
+let umuriiKonkolaachisuu = 18 - Umurii;
+
+console.log("Wagaa dhufu umuriin kee ", umuriiKeeWaggaaDhufuu, " ta'a!");
+console.log(
+  "Konkolaachisuu eegaluuf waggaa ",
+  umuriiKonkolaachisuu,
+  " tu siif hafa",
+);
